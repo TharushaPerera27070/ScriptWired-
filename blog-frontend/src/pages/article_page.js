1e1cde7a4a8b620716e70cbdcfd8bb146f71e2e9
Tharusha_Perera <134818242+TharushaPerera27070@users.noreply.github.com>
2024-09-02 19:00:32 +0530

    before adding axios

--- blog-frontend/src/pages/article_page.js
+++ blog-frontend/src/pages/article_page.js
@@ -1,8 +1,16 @@
+import { useState, useEffect } from "react";//back-end
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./not_found_page";
 
 const ArticlePage = () => {
+
+    const [articleInfo, setArticalInfo] = useState({ upvotes: 0, comments:[] }); //back-end
+    
+    useEffect(() => {
+       setArticalInfo({ upvotes: 3, comments: []});  //back-end 
+    });
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
     
@@ -12,6 +20,7 @@ const ArticlePage = () => {
     return(
     <>
        <h1>{article.title}</h1>
+       <p>This article has {articleInfo.upvotes} upvote(s)</p>
        {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
        ))}
