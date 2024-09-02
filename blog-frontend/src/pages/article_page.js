a18e6e7bd6e51aba5d241615caf52edce7f40005
Tharusha_Perera <134818242+TharushaPerera27070@users.noreply.github.com>
2024-09-02 19:25:04 +0530

    connected upvotes

--- blog-frontend/src/pages/article_page.js
+++ blog-frontend/src/pages/article_page.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";//back-end
 import { useParams } from "react-router-dom";
+import axios from 'axios';//use for request data from the back-end
 import articles from "./article-content";
 import NotFoundPage from "./not_found_page";
 
 const ArticlePage = () => {
 
-    const [articleInfo, setArticalInfo] = useState({ upvotes: 0, comments:[] }); //back-end
-    
-    useEffect(() => {
-       setArticalInfo({ upvotes: 3, comments: []});  //back-end 
-    });
-
+    const [articleInfo, setArticalInfo] = useState({ upvotes: 0, comments:[] }); // =>
     const { articleId } = useParams();
+
+    useEffect(() => {
+       const loadArticalInfo = async () => {
+        const response = await axios.get(`/api/articles/${articleId}`);
+        const newArticleInfo = response.data;
+        setArticalInfo(newArticleInfo);  
+       }
+        loadArticalInfo();
+    });                                                                          // <= back-end                         
+
+   
     const article = articles.find(article => article.name === articleId);
     
     if(!article){
